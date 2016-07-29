b5ea432383e8c763374c4201e9d13ca64c37c75f
kenfrancisadams <kenfrancisadams@users.noreply.github.com>
2016-07-29 11:38:29 +0100

    Comments and layout
    Cleaned up the code, added comments and renamed some variables

--- dates.php
+++ dates.php
@@ -2,33 +2,55 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>Untitled Document</title>
+		<title>php-weekday-table</title>
 		<style> 
+			<!-- Very basic styling for the table so you can see more easily see if the output is correct -->
 			table { border:1px solid grey;} 
 			tr td { width:10px;border:1px solid grey; }
 		</style> 
 	</head> 
-	<body>
-<?php 
-$startdate = strtotime('2017-06-05');
-$enddate = strtotime('2017-07-21');
-$difference = $enddate - $startdate;
-$number_of_days = floor($difference/(60*60*24) + 1);
-echo "<table class=\"table table-bordered table-hover table-condensed\"><tr><tr class=\"success text-center\"> <td colspan=\"8\">MONDAY</td> <td colspan=\"8\">TUESDAY</td> <td colspan=\"8\">WEDNESDAY</td> <td colspan=\"8\">THURSDAY</td> <td colspan=\"8\">FRIDAY</td> </tr><tr class=\"info\"> <td></td> <td>R</td> <td>1</td> <td>2</td> <td>3</td> <td>4</td> <td>5</td> <td>6</td> <td></td> <td>R</td> <td>1</td> <td>2</td> <td>3</td> <td>4</td> <td>5</td> <td>6</td> <td></td> <td>R</td> <td>1</td> <td>2</td> <td>3</td> <td>4</td> <td>5</td> <td>6</td> <td></td> <td>R</td> <td>1</td> <td>2</td> <td>3</td> <td>4</td> <td>5</td> <td>6</td> <td></td> <td>R</td> <td>1</td> <td>2</td> <td>3</td> <td>4</td> <td>5</td> <td>6</td> </tr>";
+<body>
+<?php
+	// Set your start date YYYY-MM-DD
+	$startdate = strtotime('2017-06-05');
+	
+	// Set your end date YYYY-MM-DD
+	$enddate = strtotime('2017-07-21');
+	
+	// Calculate the difference between the dates in seconds
+	$difference = $enddate - $startdate;
+	
+	// Convert seconds to whole days
+	$total_days = floor($difference/(60*60*24) + 1);
+	
+	// Create the unchanging table header stuff
+	echo "<table class=\"table table-bordered table-hover table-condensed\"><tr><tr class=\"success text-center\"> <td colspan=\"8\">MONDAY</td> <td colspan=\"8\">TUESDAY</td> <td colspan=\"8\">WEDNESDAY</td> <td colspan=\"8\">THURSDAY</td> <td colspan=\"8\">FRIDAY</td> </tr><tr class=\"info\"> <td></td> <td>R</td> <td>1</td> <td>2</td> <td>3</td> <td>4</td> <td>5</td> <td>6</td> <td></td> <td>R</td> <td>1</td> <td>2</td> <td>3</td> <td>4</td> <td>5</td> <td>6</td> <td></td> <td>R</td> <td>1</td> <td>2</td> <td>3</td> <td>4</td> <td>5</td> <td>6</td> <td></td> <td>R</td> <td>1</td> <td>2</td> <td>3</td> <td>4</td> <td>5</td> <td>6</td> <td></td> <td>R</td> <td>1</td> <td>2</td> <td>3</td> <td>4</td> <td>5</td> <td>6</td> </tr>";
 		
-		for ($i = 0; $i < $number_of_days; $i++) { 
-			$date = strtotime(date("Y-m-d", $startdate) . " +$i day");
-			$day_of_week = date("l", $date); 
+		// Repeat until we've gone through all the days in our range
+		for ($count = 0; $i < $total_days; $count++) { 
+			$date = strtotime(date("Y-m-d", $startdate) . " +$count day");
+			
+			// Find out what day of the week each date is (with a capital letter)
+			$day_of_week = date("l", $date);
+			
+			// If its a Saturday, no output
 			if ($day_of_week == "Saturday")	{
 			} 
+			// If it's a Sunday, close the table row, start a new one and insert a line break so the HTML is readable
 			else if ($day_of_week == "Sunday") { 
 				echo "</tr><tr>\n"; 
 			} 
+			// If it's not Saturday or Sunday then create a table cell and put the date in it
+			// Then close the cell and add, in this case, 7 more
 			else { 
+				// Create a table cell
 				echo "<td>";
-				echo date('d/m', $date); 
+				// Put the date in it
+				echo date('d/m', $date);
+				// Close the cell and add some more blank ones for the periods
 				echo "</td>\n<td></td>\n<td></td>\n<td></td>\n<td></td>\n<td></td>\n<td></td>\n<td></td>\n";
 			} 
+		// Once all the days in the range have been written to the table, close the last row and the table	
 		} echo "</tr>\n</table>\n"; ?>
-	</body>
+</body>
 </html>
